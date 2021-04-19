909352589f96588af6696f5595e428a9f055c4b1
divlo <contact@divlo.fr>
2021-04-20 01:15:07 +0200

    chore: setup semantic-release

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  moduleDirectories: ['node_modules', './'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    '@testing-library/react'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/.next/**',
+    '!**/node_modules/**',
+    '!**/next.config.js',
+    '!**/postcss.config.js',
+    '!**/workbox-*.js',
+    '!**/sw.js'
+  ],
+  coverageDirectory: './coverage',
+  coverageReporters: ['text', 'cobertura']
+}
