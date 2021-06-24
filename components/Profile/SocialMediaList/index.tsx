3ef876b737344fece47f48045a3df9a58ede9f2c
divlo <contact@divlo.fr>
2021-06-24 20:10:53 +0200

    feat: add GitLab and npm social medias like README

--- components/Profile/SocialMediaList/index.tsx
+++ components/Profile/SocialMediaList/index.tsx
@@ -1,18 +1,26 @@
 import { SocialMediaItem } from './SocialMediaItem'
 import { TwitterIcon } from './SocialMediaIcons/TwitterIcon'
 import { GitHubIcon } from './SocialMediaIcons/GitHubIcon'
+import { GitLabIcon } from './SocialMediaIcons/GitLabIcon'
 import { YouTubeIcon } from './SocialMediaIcons/YouTubeIcon'
 import { TwitchIcon } from './SocialMediaIcons/TwitchIcon'
 import { EmailIcon } from './SocialMediaIcons/EmailIcon'
+import { NPMIcon } from './SocialMediaIcons/NPMIcon'
 
 export const SocialMediaList: React.FC = () => {
   return (
     <ul className='social-media-list m-0 mt-2 py-4 list-none text-center'>
-      <SocialMediaItem link='https://twitter.com/Divlo_FR' ariaLabel='Twitter'>
-        <TwitterIcon />
-      </SocialMediaItem>
       <SocialMediaItem link='https://github.com/Divlo' ariaLabel='GitHub'>
         <GitHubIcon />
+      </SocialMediaItem>
+      <SocialMediaItem link='https://gitlab.com/Divlo' ariaLabel='GitLab'>
+        <GitLabIcon />
+      </SocialMediaItem>
+      <SocialMediaItem link='https://www.npmjs.com/~divlo' ariaLabel='NPM'>
+        <NPMIcon />
+      </SocialMediaItem>
+      <SocialMediaItem link='https://twitter.com/Divlo_FR' ariaLabel='Twitter'>
+        <TwitterIcon />
       </SocialMediaItem>
       <SocialMediaItem
         link='https://www.youtube.com/c/Divlo'
