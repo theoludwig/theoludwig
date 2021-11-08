4b85cfe9e247df2b97680f50125a33f26314fc1b
Divlo <contact@divlo.fr>
2021-11-08 15:10:26 +0100

    feat: add blog (#320)

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,77 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { useMemo } from 'react'
+import { getMDXComponent } from 'mdx-bundler/client'
+import date from 'date-and-time'
+import 'prism-themes/themes/prism-one-dark.css'
+
+import { Head } from 'components/Head'
+import { Header } from 'components/Header'
+import { Footer, FooterProps } from 'components/Footer'
+import { Post } from 'utils/blog'
+
+interface BlogPostPageProps extends FooterProps, Post {}
+
+const BlogPostPage: React.FC<BlogPostPageProps> = (props) => {
+  const { version, code, frontmatter } = props
+
+  const Component = useMemo(() => getMDXComponent(code), [code])
+
+  return (
+    <>
+      <Head
+        title={`${frontmatter.title} | Divlo`}
+        description={frontmatter.description}
+      />
+
+      <Header />
+      <main className='flex flex-col flex-wrap flex-1 items-center'>
+        <div className='flex flex-col items-center my-10'>
+          <h1 className='text-3xl font-semibold'>{frontmatter.title}</h1>
+          <p className='mt-2' data-cy='blog-post-date'>
+            {date.format(new Date(frontmatter.publishedOn), 'DD/MM/YYYY')}
+          </p>
+        </div>
+        <div className='prose mb-10 px-8'>
+          <Component
+            components={{
+              a: (props) => (
+                <a target='_blank' rel='noopener noreferrer' {...props} />
+              )
+            }}
+          />
+        </div>
+      </main>
+      <Footer version={version} />
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps<BlogPostPageProps> = async (
+  context
+) => {
+  const slug = context?.params?.slug
+  const post = await Post.getBySlug(slug)
+  if (post == null || (post != null && !post.frontmatter.isPublished)) {
+    return {
+      redirect: {
+        destination: '/404',
+        permanent: false
+      }
+    }
+  }
+  const { readPackage } = await import('read-pkg')
+  const { version } = await readPackage()
+  return { props: { version, ...post } }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await Post.getMany()
+  return {
+    paths: posts.map((post) => {
+      return { params: { slug: post.slug } }
+    }),
+    fallback: false
+  }
+}
+
+export default BlogPostPage
---
+++ utils/blog.ts
@@ -0,0 +1,97 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
+import { bundleMDXFile } from 'mdx-bundler'
+import remarkGfm from 'remark-gfm'
+import remarkPrism from 'remark-prism'
+
+export const postsPath = path.join(process.cwd(), 'posts')
+
+if (process.platform === 'win32') {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'esbuild.exe'
+  )
+} else {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'bin',
+    'esbuild'
+  )
+}
+
+export interface FrontMatter {
+  title: string
+  description: string
+  isPublished: boolean
+  publishedOn: string
+}
+
+export interface PostOptions {
+  code: string
+  frontmatter: FrontMatter
+  slug: string
+}
+
+export class Post implements PostOptions {
+  public code: string
+  public frontmatter: FrontMatter
+  public slug: string
+
+  constructor(options: PostOptions) {
+    this.code = options.code
+    this.frontmatter = options.frontmatter
+    this.slug = options.slug
+  }
+
+  static async build(slug: string): Promise<Post> {
+    const bundledMdx = await bundleMDXFile(
+      path.join(postsPath, `${slug}.mdx`),
+      {
+        xdmOptions(options) {
+          options.remarkPlugins = [
+            ...(options.remarkPlugins ?? []),
+            remarkGfm,
+            remarkPrism as any
+          ]
+          return options
+        }
+      }
+    )
+    const frontmatter = bundledMdx.frontmatter as FrontMatter
+    return {
+      code: bundledMdx.code,
+      frontmatter,
+      slug
+    }
+  }
+
+  static async getBySlug(slug?: string | string[]): Promise<Post | undefined> {
+    const posts = await Post.getMany()
+    const post = posts.find((post) => post.slug === slug)
+    return post
+  }
+
+  static async getMany(): Promise<Post[]> {
+    const posts = await fs.promises.readdir(postsPath)
+    const postsWithTime = await Promise.all(
+      posts.map(async (postFilename) => {
+        const [slug] = postFilename.split('.')
+        const post = await Post.build(slug)
+        const date = new Date(post.frontmatter.publishedOn)
+        return {
+          ...post,
+          time: date.getTime()
+        }
+      })
+    )
+    const postsWithTimeSorted = postsWithTime
+      .filter((post) => post.frontmatter.isPublished)
+      .sort((a, b) => b.time - a.time)
+    return postsWithTimeSorted
+  }
+}
