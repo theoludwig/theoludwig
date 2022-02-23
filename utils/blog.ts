33078ece667630f8e8962925c87b89003190c069
Divlo <contact@divlo.fr>
2022-02-23 09:06:12 +0100

    chore: temporarily support Node.js v14 to deploy on Vercel

--- utils/blog.ts
+++ utils/blog.ts
@@ -1,5 +1,5 @@
-import fs from 'node:fs'
-import path from 'node:path'
+import fs from 'fs'
+import path from 'path'
 
 import type { MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { nodeTypes } from '@mdx-js/mdx'
