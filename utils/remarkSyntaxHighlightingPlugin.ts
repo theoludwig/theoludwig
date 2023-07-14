bc719578d2937e0f0db4e5c36b2f306439f52950
Théo LUDWIG <contact@theoludwig.fr>
2023-07-14 23:50:20 +0200

    fix: remove vercel cli + update dependencies to latest

--- utils/remarkSyntaxHighlightingPlugin.ts
+++ utils/remarkSyntaxHighlightingPlugin.ts
@@ -1,5 +1,5 @@
 import type { Plugin, Transformer } from 'unified'
-import type { Literal } from 'unist'
+import type { Literal, Node } from 'unist'
 import { visit } from 'unist-util-visit'
 import type { Highlighter } from 'shiki'
 
@@ -7,15 +7,17 @@ export interface RemarkSyntaxHighlightingPluginOptions {
   highlighter: Highlighter
 }
 
-export type RemarkSyntaxHighlightingNode = Literal<string> & {
+export interface RemarkSyntaxHighlightingNode extends Node {
   lang: string
   meta: string
   children: undefined
+  value: string
+  data: Record<string, unknown>
 }
 
 export const remarkSyntaxHighlightingPlugin: Plugin<
   [RemarkSyntaxHighlightingPluginOptions],
-  Literal<string, RemarkSyntaxHighlightingNode>
+  Literal
 > = (options) => {
   const transformer: Transformer<RemarkSyntaxHighlightingNode> = (tree) => {
     visit<RemarkSyntaxHighlightingNode, string>(tree, 'code', (node) => {
