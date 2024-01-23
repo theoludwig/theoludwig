a89b5932c24c500ad17bfeafda8bdecf15f2402b
Théo LUDWIG <contact@theoludwig.fr>
2024-01-23 22:01:50 +0100

    fix: update dependencies to latest

--- components/Profile/ProfileLogo.tsx
+++ components/Profile/ProfileLogo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-import Logo from "public/images/logo.png"
+import Logo from "@/public/images/logo.png"
 
 export const ProfileLogo = (): JSX.Element => {
   return (
