2897d181c554d9813830b1ff3fe3d5611df0c5eb
Théo LUDWIG <contact@theoludwig.fr>
2024-07-20 10:26:19 +0200

    Revert "fix: stop using flag image, use emoji instead"
    This reverts commit f64acb68c72869a7461757668d5c4c0ceb30ee96.

--- components/Header/Locales/LocaleFlag.tsx
+++ components/Header/Locales/LocaleFlag.tsx
@@ -1,5 +1,7 @@
-import { useI18n } from "@/i18n/i18n.client"
+import Image from "next/image"
+
 import type { CookiesStore } from "@/utils/constants"
+import { useI18n } from "@/i18n/i18n.client"
 
 export interface LocaleFlagProps {
   locale: string
@@ -12,8 +14,17 @@ export const LocaleFlag = (props: LocaleFlagProps): JSX.Element => {
   const i18n = useI18n(cookiesStore)
 
   return (
-    <p data-cy="locale-flag-text" className="mx-2 text-lg font-semibold">
-      {i18n.translate(`common.${locale}`)}
-    </p>
+    <>
+      <Image
+        quality={100}
+        width={35}
+        height={35}
+        src={`/images/locales/${locale}.svg`}
+        alt={locale}
+      />
+      <p data-cy="locale-flag-text" className="mx-2 text-base">
+        {i18n.translate(`common.${locale}`)}
+      </p>
+    </>
   )
 }
