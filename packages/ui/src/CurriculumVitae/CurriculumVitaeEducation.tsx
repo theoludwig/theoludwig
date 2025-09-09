92eca82127d54a97c089be62125b1fd02ebca2c0
Théo LUDWIG <contact@theoludwig.fr>
2025-09-09 19:32:06 +0200

    fix: update Curriculum Vitae

--- packages/ui/src/CurriculumVitae/CurriculumVitaeEducation.tsx
+++ packages/ui/src/CurriculumVitae/CurriculumVitaeEducation.tsx
@@ -14,50 +14,55 @@ export const CurriculumVitaeEducation: React.FC<
       years: t("curriculum-vitae.education.cnam.years.2024-2027.title"),
       studyType: t("curriculum-vitae.education.cnam.study-type"),
       institution: t("curriculum-vitae.education.cnam.institution"),
-      score: t("curriculum-vitae.education.cnam.years.2024-2027.description"),
+      // score: t("curriculum-vitae.education.cnam.years.2024-2027.description"),
       courses: [],
     },
     {
-      years: t("curriculum-vitae.education.iut.years.2023-2024.title"),
+      years: t("curriculum-vitae.education.iut.years.title"),
       studyType: t("curriculum-vitae.education.iut.study-type"),
       institution: t("curriculum-vitae.education.iut.institution"),
-      score: t("curriculum-vitae.education.iut.years.2023-2024.description"),
       courses: [
-        t("curriculum-vitae.education.iut.years.2023-2024.courses.web"),
-        t("curriculum-vitae.education.iut.years.2023-2024.courses.ci-cd"),
-        t(
-          "curriculum-vitae.education.iut.years.2023-2024.courses.complexity-algorithms",
-        ),
-        t("curriculum-vitae.education.iut.years.2023-2024.courses.no-sql"),
-      ],
-    },
-    {
-      years: t("curriculum-vitae.education.iut.years.2022-2023.title"),
-      studyType: t("curriculum-vitae.education.iut.study-type"),
-      institution: t("curriculum-vitae.education.iut.institution"),
-      score: t("curriculum-vitae.education.iut.years.2022-2023.description"),
-      courses: [
-        t("curriculum-vitae.education.iut.years.2022-2023.courses.web"),
-        t("curriculum-vitae.education.iut.years.2022-2023.courses.tests"),
-        t("curriculum-vitae.education.iut.years.2022-2023.courses.clean-code"),
-        t("curriculum-vitae.education.iut.years.2022-2023.courses.systems-c"),
-        // t(
-        //   "curriculum-vitae.education.iut.years.2022-2023.courses.sql-security",
-        // ),
-      ],
-    },
-    {
-      years: t("curriculum-vitae.education.iut.years.2021-2022.title"),
-      studyType: t("curriculum-vitae.education.iut.study-type"),
-      institution: t("curriculum-vitae.education.iut.institution"),
-      score: t("curriculum-vitae.education.iut.years.2021-2022.description"),
-      courses: [
-        t("curriculum-vitae.education.iut.years.2021-2022.courses.java"),
-        t("curriculum-vitae.education.iut.years.2021-2022.courses.systems-c"),
-        // t(
-        //   "curriculum-vitae.education.iut.years.2021-2022.courses.windows-forms",
-        // ),
-        t("curriculum-vitae.education.iut.years.2021-2022.courses.sql"),
+        {
+          year: t("curriculum-vitae.education.iut.years.2021-2022.description"),
+          title: t("curriculum-vitae.education.iut.years.2021-2022.title"),
+          courses: [
+            t("curriculum-vitae.education.iut.years.2021-2022.courses.java"),
+            t(
+              "curriculum-vitae.education.iut.years.2021-2022.courses.systems-c",
+            ),
+            // t(
+            //   "curriculum-vitae.education.iut.years.2021-2022.courses.windows-forms",
+            // ),
+            t("curriculum-vitae.education.iut.years.2021-2022.courses.sql"),
+          ],
+        },
+        {
+          year: t("curriculum-vitae.education.iut.years.2022-2023.description"),
+          title: t("curriculum-vitae.education.iut.years.2022-2023.title"),
+          courses: [
+            t("curriculum-vitae.education.iut.years.2022-2023.courses.web"),
+            t("curriculum-vitae.education.iut.years.2022-2023.courses.tests"),
+            t(
+              "curriculum-vitae.education.iut.years.2022-2023.courses.clean-code",
+            ),
+            // t("curriculum-vitae.education.iut.years.2022-2023.courses.sql-security"),
+            t(
+              "curriculum-vitae.education.iut.years.2022-2023.courses.systems-c",
+            ),
+          ],
+        },
+        {
+          year: t("curriculum-vitae.education.iut.years.2023-2024.description"),
+          title: t("curriculum-vitae.education.iut.years.2023-2024.title"),
+          courses: [
+            t("curriculum-vitae.education.iut.years.2023-2024.courses.web"),
+            t("curriculum-vitae.education.iut.years.2023-2024.courses.ci-cd"),
+            t(
+              "curriculum-vitae.education.iut.years.2023-2024.courses.complexity-algorithms",
+            ),
+            t("curriculum-vitae.education.iut.years.2023-2024.courses.no-sql"),
+          ],
+        },
       ],
     },
     // {
@@ -83,9 +88,13 @@ export const CurriculumVitaeEducation: React.FC<
                 <strong>{education.studyType}</strong>
               </p>
 
-              <p className="relative m-0">
-                <strong>{education.score}</strong>
-              </p>
+              {/* {education.score != null ? (
+                <p className="relative m-0">
+                  <strong>{education.score}</strong>
+                </p>
+              ) : (
+                <></>
+              )} */}
 
               <p className="text-muted m-0">{education.institution}</p>
 
@@ -94,13 +103,23 @@ export const CurriculumVitaeEducation: React.FC<
               </p>
 
               {education.courses.length > 0 ? (
-                <ul
-                  style={{
-                    paddingInlineStart: 20,
-                  }}
-                >
-                  {education.courses.map((course) => {
-                    return <li key={course}>{course}</li>
+                <ul className="list-none">
+                  {education.courses.map(({ year, title, courses }) => {
+                    return (
+                      <li key={year}>
+                        <span className="font-medium">{year}</span>
+                        <span> ({title})</span>
+                        <ul
+                          style={{
+                            paddingInlineStart: 20,
+                          }}
+                        >
+                          {courses.map((course) => {
+                            return <li key={course}>{course}</li>
+                          })}
+                        </ul>
+                      </li>
+                    )
                   })}
                 </ul>
               ) : (
